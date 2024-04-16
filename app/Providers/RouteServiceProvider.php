00ed6a7bccfbfa9c86ae8dddcae4b43a479b09e9
tinggithub0 <66715785+tinggithub0@users.noreply.github.com>
2024-04-16 23:42:37 +0800

    微調

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,8 +33,11 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
-            Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+                Route::middleware('web')
+                ->group([
+                    base_path('routes/web.php'),
+                    base_path('routes/auth.php'),
+                ]);
         });
     }
 }
